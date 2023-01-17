60d41c1b5b39ad02f618715893807a58cf1682b0
moksudahmed <k.moksud@gmail.com>
2023-01-17 11:22:07 +0600

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,21 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import * as path from 'path'
 import { Configuration, OpenAIApi } from 'openai'
+import bodyParser from "body-parser";
 
 dotenv.config()
-//dotenv.config({path:'relative/path/server/.env'});
+console.log(process.env.OPENAI_API_KEY)
 const result = dotenv.config()
 
 if (result.error) {
   throw result.error
 }
-
 //console.log(result.parsed)
 //console.log(process.env.OPENAI_API_KEY)
 
 const configuration = new Configuration({
-  apiKey: "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",
+  apiKey: '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',
 });
 
 const openai = new OpenAIApi(configuration);
@@ -24,6 +23,8 @@ const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', async (req, res) => {
   res.status(200).send({
@@ -32,10 +33,10 @@ app.get('/', async (req, res) => {
 })
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
+    const prompt = req.body.prompt;    
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `${prompt}`,
+      prompt: prompt,
       temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 3000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
